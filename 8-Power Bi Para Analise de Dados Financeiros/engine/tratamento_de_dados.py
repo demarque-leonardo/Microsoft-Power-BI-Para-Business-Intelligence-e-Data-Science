6c4ed448a85cf5cad8b29bdb0e21001aa037f850
demarque-leonardo <demarque.leonardo@gmail.com>
2026-08-10 20:11:50 -0300

    Update tratamento_de_dados.py

--- 8-Power Bi Para Analise de Dados Financeiros/engine/tratamento_de_dados.py
+++ 8-Power Bi Para Analise de Dados Financeiros/engine/tratamento_de_dados.py
@@ -1,6 +1,9 @@
 import pandas as pd
 
+# Importando o set de dados de um arquivo excel
 df = pd.read_excel(r"8-Power Bi Para Analise de Dados Financeiros\database\DadosFinanceiros.xlsx")
 
+# Identificando o schema da database
 print(df)
 
+colunas_fixas = ["Tipo", "Componente"]
